00546f1a7b60032f7f645127cd6ba574583f3493
Meg McCauley <megan@mccauley.net>
2015-11-15 11:19:09 -0500

    Small changes on v1

--- sense_and_move/sense_and_move.ino
+++ sense_and_move/sense_and_move.ino
@@ -5,13 +5,14 @@
 Servo myservo;  // create servo object to control a servo 
                 // twelve servo objects can be created on most boards
  
-int pos = 0;    // variable to store the servo position
-int sensorPin = A0;    // select the input pin for the potentiometer
-int sensorValue = 0;  // variable to store the value coming from the sensor
+int pos = 0;            // variable to store the servo position
+int sensorPin = A0;     // sensor 1
+int servoPin = 7;       // servo 1
+int sensorValue = 0;    // variable to store the value coming from the sensor
  
 void setup() 
 { 
-  myservo.attach(7);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
   Serial.begin(9600);
 } 
  
