b4236b52c810e492a568c49d056c3deebc9dccd3
Bonnie Ishiguro <bgishiguro@gmail.com>
2015-11-17 12:02:18 -0500

    90 degree sweep, renaming 0-90/90-0 sweep

---
+++ sense_sweep-90/sense_sweep-90/sense_sweep-90.ino
@@ -0,0 +1,45 @@
+/*sense_and_move_v2
+Don't read sensor values until finished with 0-90 sweep.
+Sweep back to zero.*/
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+int servoPin = 9;         // servo 1
+
+int sensorPin = A0;       // sensor 1
+int sensorValue = 0;      // variable to store the value coming from the sensor
+
+boolean zero_start = true;
+
+void setup() 
+{ 
+  myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
+} 
+ 
+void loop() 
+{
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  
+  if(sensorValue > 100) { // if the sensor detects an object
+
+    if (zero_start) {
+      for(int i = 0; i < 90; i+=1) { // turn 90 degrees so that the paddle appears thick
+        myservo.write(i);
+        delay(10);
+      }
+      delay(20);
+      
+    }
+    else {
+      for(int i = 90; i >=0; i-=1) { // turn 90 degrees back so that the paddle appears thin
+        myservo.write(i);
+        delay(10);
+      }
+      delay(20); 
+    }
+    zero_start = !zero_start;
+  }
+}
