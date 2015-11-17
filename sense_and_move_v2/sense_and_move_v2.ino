ab8548865273099fccfc529862c1c06299dd4dad
Bonnie Ishiguro <bgishiguro@gmail.com>
2015-11-17 11:51:44 -0500

    Sweep from 0-90 and back from 90-0 in response to sensor input

--- sense_and_move_v2/sense_and_move_v2.ino
+++ sense_and_move_v2/sense_and_move_v2.ino
@@ -5,13 +5,11 @@ Sweep back to zero.*/
 #include <Servo.h> 
  
 Servo myservo;  // create servo object to control a servo 
- 
-int pos = 0;              // variable to store the servo position
+int servoPin = 9;         // servo 1
+
 int sensorPin = A0;       // sensor 1
-int servoPin = 7;         // servo 1
 int sensorValue = 0;      // variable to store the value coming from the sensor
-boolean sensing = true;   // boolean value, true if currerntly sensing
- 
+
 void setup() 
 { 
   myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
@@ -19,31 +17,21 @@ void setup()
 } 
  
 void loop() 
-{ 
-  Serial.println(sensorValue);
-  if (sensing){
+{
   sensorValue = analogRead(sensorPin);
-  //Serial.println(sensorValue);
-  }
+  Serial.println(sensorValue);
   
-  //might need to add something in to delay and/or reset to zero to prevent jerkiness
-  
-  if(sensorValue > 80) {
-    
-    sensing = false;
-    sensorValue = 0;
-    
-    for(int i = 0; i < 90; i+=1) {
+  if(sensorValue > 100) { // if the sensor detects an object
+    for(int i = 0; i < 90; i+=1) { // turn 90 degrees so that the paddle appears thick
       myservo.write(i);
       delay(10);
     }
     delay(20);
-    for(int i = 90; i >=0; i-=1) {
+    
+    for(int i = 90; i >=0; i-=1) { // turn 90 degrees back so that the paddle appears thin
       myservo.write(i);
       delay(10);
     }
-    
-    sensing = true;
+    delay(20);
   }
 }
-
