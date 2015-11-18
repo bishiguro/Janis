965c92d3213e2f6ab06fbd85c7f4a4786fb8a08e
Bonnie Ishiguro <bgishiguro@gmail.com>
2015-11-17 22:56:05 -0500

    90 Sweep Multiple Servos, In Progress

--- sense_sweep-90/sense_sweep-90.ino
+++ sense_sweep-90/sense_sweep-90.ino
@@ -3,25 +3,41 @@ Don't read sensor values until finished with 0-90 sweep.
 Sweep back to zero.*/
 
 #include <Servo.h> 
- 
-Servo myservo;  // create servo object to control a servo 
-int servoPin = 9;         // servo 1
 
-int sensorPin = A0;       // sensor 1
-int sensorValue = 0;      // variable to store the value coming from the sensor
+const int NUM_SERVOS = 1;
+const int NUM_SENSORS = 1;
+
+//Servo myservo;  // create servo object to control a servo 
+//int servoPin = 9;         // servo 1
+
+Servo servos[NUM_SERVOS];
+int servo_pins[] = {9};
+
+int sensor_pins[] = {A0};
+int sensor_values = {0};
+
+//int sensorPin = A0;       // sensor 1
+//int sensorValue = 0;      // variable to store the value coming from the sensor
 
 boolean zero_start = true;
 
 void setup() 
 { 
-  myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
+  //myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
+  for (int i = 0; i < NUM_SERVOS; i++) {
+    servos[i].attach(servo_pins[i]);
+  }
+  
   Serial.begin(9600);
 } 
  
 void loop() 
 {
-  sensorValue = analogRead(sensorPin);
-  Serial.println(sensorValue);
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    sensor_values[i] = analogRead(sensor_pins[i]);
+  }
+  //sensorValue = analogRead(sensorPin);
+  //Serial.println(sensorValue);
   
   if(sensorValue > 100) { // if the sensor detects an object
 
