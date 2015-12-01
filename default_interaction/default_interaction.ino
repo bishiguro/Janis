d06727004d80d38ae0dddd08afb62d08d895a5e4
shrinidhit <shrinidhi.thirumalai@students.olin.edu>
2015-12-01 01:09:10 -0500

    Adjusted main loop to prevent event queue overflow
    Adjusted the main loop structure so new default pattern events are only
    added as the event queue becomes empty.

--- default_interaction/default_interaction.ino
+++ default_interaction/default_interaction.ino
@@ -1,6 +1,6 @@
 /*Default Interaction
 
-Pleasing wave pattern that runs while nobody is interacting with the installation 
+Description: A Pleasing wave pattern that runs while nobody is interacting with the installation 
 Iteration 1: Simple wave runs forever (one paddle moves, followed by the next)
 Iteration 2: Wave runs forever, but with random frequencies or amplitudes (still need to brainstorm what can randomly change)
 Stretch Idea: Wave runs forever, but tells time/ reacts to time (There are 12 paddles and 12 hours, so something interesting could happen here> 
@@ -27,19 +27,9 @@ typedef struct {
 //create queue for storing and deploying all sensor events
 QueueArray <servoEvent> queue;
 
-//---------------------------------------------------------------Functions---------------------------------------------------------------
-
-//Setup
-void setup() 
-{
-  for (int i = 0; i < NUM_SERVOS; i++) { //loops through all sensors
-      servos[i].attach(servoPins[i]);  // attaches the servo on pin 9 to the servo object  
-  }
-  Serial.begin(9600); 
-} 
-
-//Helper Functions
+//---------------------------------------------------------------Helper Functions---------------------------------------------------------------
 void move_forward(int servoIndex) {
+//Move servo forward from 0 to 90 degrees
   for(int i = 0; i < 90; i+=1) { // turn 90 degrees so that the paddle appears thick
     servos[servoIndex].write(i);
     delay(10);
@@ -49,6 +39,7 @@ void move_forward(int servoIndex) {
 }
 
 void move_backward(int servoIndex) {
+//Move servo backwards from 90 to 0 degrees
   for(int i = 90; i >=0; i-=1) { // turn 90 degrees back so that the paddle appears thin
     servos[servoIndex].write(i);
     delay(10);
@@ -57,45 +48,51 @@ void move_backward(int servoIndex) {
 }
 
 void update_servos() {
-//Update servos with event in queue
-
+//Release one event in the queue
   if (!queue.isEmpty()) {
     servoEvent event = queue.dequeue(); //pops last element
     move_forward(event.pin); //moves servo
   }
 }
 
-bool if_special_time() {
-	//check if time is special. If it is, return true
+bool is_special_time() {
+	//Check if time is special. If it is, return true
 }
 
-void time_pattern() {
-	//runs pattern that happens for a special time
+void add_time_pattern() {
+	//Creates a sequence that happens for a special time, and adds it to the queue
 
 }
 
-void default_pattern() {
-	//loops through a single cycle of some default pattern, and adds to the queue
+void add_default_pattern() {
+	//Creates a single cycle of some default pattern, and adds it to the queue
 }
 
 void empty_queue() {
 
 }
 
-//main loop.
-void loop()  
-//We need to do some major thinking on how time should govern this loop
-//We still need to adjust so th at it runs for the same length of time each loop. 
-//We also need to make sure time_pattern and default_pattern don't enqueue way more than update_servos can dequeue with each loop. 
-//Perhaps loop priorities, with time patterns having a high priority? Or we could just empty the entire queue when a "special time" happens (That's how the code is set up now).
+//---------------------------------------------------------------Setup and Main Loop---------------------------------------------------------------
+//Setup
+void setup() {
+  for (int i = 0; i < NUM_SERVOS; i++) { //loops through all sensors
+      servos[i].attach(servoPins[i]);  // attaches the servo on pin 9 to the servo object  
+  }
+  Serial.begin(9600); 
+} 
 
-{
-	if (if_special_time()) {
+//Main loop.
+void loop() {
+//We need to do some major thinking on how time should govern this loop
+//We still need to adjust so that it runs for the same length of time each loop. 
+//We also need to make sure time_pattern and default_pattern don't enqueue way more than update_servos can dequeue with each loop. 
+//Perhaps loop priorities, with time patterns having a high priority? Or we could just empty the entire queue when a "special time" happens (That's how the code is set up now)
+	if (is_special_time()) {
 		empty_queue();
-		time_pattern();
+		add_time_pattern();
 	}
-	else {
-		default_pattern();
+	else if (queue.isEmpty()) {
+		add_default_pattern();
 	}
 	update_servos();
 }
