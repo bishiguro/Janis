04b1b351fb7159dae84219b581c3264fea30414c
shrinidhit <shrinidhi.thirumalai@students.olin.edu>
2015-12-01 00:22:52 -0500

    Default interaction skeleton code + some setup

---
+++ default_interaction/default_interaction.ino
@@ -0,0 +1,101 @@
+/*Default Interaction
+
+Pleasing wave pattern that runs while nobody is interacting with the installation 
+Iteration 1: Simple wave runs forever (one paddle moves, followed by the next)
+Iteration 2: Wave runs forever, but with random frequencies or amplitudes (still need to brainstorm what can randomly change)
+Stretch Idea: Wave runs forever, but tells time/ reacts to time (There are 12 paddles and 12 hours, so something interesting could happen here> 
+OR, perhaps like a cuckoo clock, a wave pattern appears on the hour, every hour)*/
+
+//---------------------------------------------------------------Libraries and Packages---------------------------------------------------------------
+#include <QueueArray.h>
+#include <Servo.h>
+// #include <Time.h>  //(http://playground.arduino.cc/code/time)
+//perhaps look into Millis, which gives you time since board has run (https://www.arduino.cc/en/Reference/Millis)
+
+//---------------------------------------------------------------Initializations---------------------------------------------------------------
+long int CURRENT_TIME;
+const int NUM_SERVOS = 12;
+Servo servos[NUM_SERVOS];  // create servo object to control a servo 
+int servoPins[NUM_SERVOS] = {9, 8, 11};         // servo 1 [NEED TO UPDATE]
+
+//create structure for storing sensor events. [pin(where pin is the servo index, not the pin it corresponds to on the arduino), value]
+typedef struct {
+  int pin;
+  int val;
+} servoEvent;
+
+//create queue for storing and deploying all sensor events
+QueueArray <servoEvent> queue;
+
+//---------------------------------------------------------------Functions---------------------------------------------------------------
+
+//Setup
+void setup() 
+{
+  for (int i = 0; i < NUM_SERVOS; i++) { //loops through all sensors
+      servos[i].attach(servoPins[i]);  // attaches the servo on pin 9 to the servo object  
+  }
+  Serial.begin(9600); 
+} 
+
+//Helper Functions
+void move_forward(int servoIndex) {
+  for(int i = 0; i < 90; i+=1) { // turn 90 degrees so that the paddle appears thick
+    servos[servoIndex].write(i);
+    delay(10);
+  }
+  Serial.print("Moved: "); Serial.println(servoIndex);
+  delay(20);
+}
+
+void move_backward(int servoIndex) {
+  for(int i = 90; i >=0; i-=1) { // turn 90 degrees back so that the paddle appears thin
+    servos[servoIndex].write(i);
+    delay(10);
+  }
+  delay(20); 
+}
+
+void update_servos() {
+//Update servos with event in queue
+
+  if (!queue.isEmpty()) {
+    servoEvent event = queue.dequeue(); //pops last element
+    move_forward(event.pin); //moves servo
+  }
+}
+
+bool if_special_time() {
+	//check if time is special. If it is, return true
+}
+
+void time_pattern() {
+	//runs pattern that happens for a special time
+
+}
+
+void default_pattern() {
+	//loops through a single cycle of some default pattern, and adds to the queue
+}
+
+void empty_queue() {
+
+}
+
+//main loop.
+void loop()  
+//We need to do some major thinking on how time should govern this loop
+//We still need to adjust so th at it runs for the same length of time each loop. 
+//We also need to make sure time_pattern and default_pattern don't enqueue way more than update_servos can dequeue with each loop. 
+//Perhaps loop priorities, with time patterns having a high priority? Or we could just empty the entire queue when a "special time" happens (That's how the code is set up now).
+
+{
+	if (if_special_time()) {
+		empty_queue();
+		time_pattern();
+	}
+	else {
+		default_pattern();
+	}
+	update_servos();
+}
