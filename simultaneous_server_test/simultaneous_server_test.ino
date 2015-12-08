ba825ead6c56a7728bcfef9bd3e08e12c4466d2a
shrinidhit <shrinidhi.thirumalai@students.olin.edu>
2015-12-07 20:34:47 -0500

    Interactive code for 2 sensors, works. :)

--- simultaneous_server_test/simultaneous_server_test.ino
+++ simultaneous_server_test/simultaneous_server_test.ino
@@ -7,31 +7,31 @@
 const int POS_MAX = 90;  //how many increments it takes to get from one position to another
 const int NUM_SERVOS = 2;
 const int NUM_SENSORS = 2;
-// int pos1[POS_MAX];
-// int pos2[POS_MAX];
 
 Servo servos[NUM_SERVOS];
 int pos[NUM_SERVOS] = {0, 0};
-int servoPins[NUM_SERVOS] = {11, 12};
-int sensorPins[NUM_SENSORS] = {A0, A1};
+int servoPins[NUM_SERVOS] = {3, 4};
+int sensorPins[NUM_SENSORS] = {A4, A5};
 int sensorValues[NUM_SENSORS] = {0, 0};
 
 void setup()
 {
   for (int i = 0; i < NUM_SERVOS; i++) { //loops through all sensors
     servos[i].attach(servoPins[i]);  // attaches the servo on pin 9 to the servo object
+    servos[i].write(0);
   }
   Serial.begin(9600);
 }
 
 void writeToServo(int sensorNum) {
-  sensorValues[sensorNum] = analogRead(sensorPins[0]);
-  int servoNum = (sensorNum*2) + 2;
+  sensorValues[sensorNum] = analogRead(sensorPins[sensorNum]);
+  // int servoNum = (sensorNum*2) + 2;
+  int servoNum = sensorNum;
 
-  if ((sensorValues[sensorNum] > 80) && (pos[servoNum] < POS_MAX)) {
+  if ((sensorValues[sensorNum] > 150) && (pos[servoNum] < POS_MAX)) {
       pos[servoNum] += 1;
   }
-  else if ((sensorValues[sensorNum] > 80) && (pos[servoNum] >= POS_MAX)) {
+  else if ((sensorValues[sensorNum] > 150) && (pos[servoNum] >= POS_MAX)) {
     pos[servoNum] = 90;
   }
   else {
@@ -42,7 +42,7 @@ void writeToServo(int sensorNum) {
 
 void loop()
 {
-  for (int i = 0; i<=NUM_SENSORS; i++) {
+  for (int i = 0; i< NUM_SENSORS; i++) {
     writeToServo(i);
   }
   delay(10);                           // waits for the servo to get there
