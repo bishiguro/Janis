149ee6e3a5aa490e5c79c3ea0875c25880dc7f1d
Bonnie Ishiguro <bgishiguro@gmail.com>
2015-12-07 22:56:22 -0500

    Sweep back and forth first attempt

--- simultaneous_server_test/simultaneous_server_test.ino
+++ simultaneous_server_test/simultaneous_server_test.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#include <servoStruct.h>
 
 const int POS_MAX = 90;  //how many increments it takes to get from one position to another
 const int NUM_SERVOS = 12;
@@ -9,27 +10,58 @@ int pos[NUM_SERVOS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 int servoPins[NUM_SERVOS] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 int sensorPins[NUM_SENSORS] = {A5, A4, A3, A2, A1, A0};
 int sensorValues[NUM_SENSORS] = {0, 0, 0, 0, 0, 0};
+ServoUnit servoUnits[NUM_SERVOS];
+
+ServoUnit createServo(int pin) {
+  ServoUnit myServo;
+  myServo.reverse = 0;
+  myServo.pos = 0;
+  myServo.pin = pin;
+  return myServo;
+}
 
 void setup()
 {
-  for (int i = 0; i < NUM_SERVOS; i++) { //loops through all sensors
-    servos[i].attach(servoPins[i]);  // attaches the servo on pin 9 to the servo object
+  for (int i = 0; i < NUM_SERVOS; i++) {
+    servoUnits[i] = createServo(servoPins[i]);
+  }
+  
+  for (int i = 0; i < NUM_SERVOS; i++) {
+    servos[i].attach(servoPins[i]);
     servos[i].write(0);
   }
+
   Serial.begin(9600);
+}
+
+bool ifChangeState(int servoIndex) {
+  ServoUnit myServo = servoUnits[servoIndex];
+  bool forward = ((myServo.reverse) && (myServo.pos > 0));
+  bool backward = ((!myServo.reverse) && (myServo.pos < 90));
+  return ! (forward || backward);
 }
 
 void getServoResponse(int sensorNum) {
   sensorValues[sensorNum] = analogRead(sensorPins[sensorNum]);
+  ServoUnit myServo = servoUnits[sensorNum];
   int servoNum = sensorNum * 2;
 
-  if ((sensorValues[sensorNum] > 150) && (pos[servoNum] < POS_MAX)) {
-    writeToServo(servoNum, pos[servoNum] + 1);
+  if (sensorValues[sensorNum] > 150) { // if the sensor is reading a value
+
+    if (ifChangeState(servoNum)) {
+      myServo.reverse = ! myServo.reverse;
+    }
+
+    if (myServo.reverse) {
+      writeToServo(servoNum, pos[servoNum] - 1);
+    }
+    else {
+      writeToServo(servoNum, pos[servoNum] + 1);
+    }
+
   }
-  else if ((sensorValues[sensorNum] > 150) && (pos[servoNum] >= POS_MAX)) {
-    writeToServo(servoNum, 90);
-  }
-  else {
+  
+  else { // move the servo back to the zero position
     writeToServo(servoNum, 0);
   }
 }
@@ -45,8 +77,8 @@ void writeToServo(int servoNum, int servoPos) {
 void loop()
 {
   for (int i = 0; i< NUM_SENSORS; i++) {
-    getSensorResponse(i);
+    getServoResponse(i);
   }
-  delay(10);                           // waits for the servo to get there
-  Serial.println(pos[0]);
+  delay(10); // waits for the servo to get there
+  //Serial.println(pos[0]);
 }
