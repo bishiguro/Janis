9fffda5201fbb9e777dedb86f9c4bae24628b76c
shrinidhit <shrinidhi.thirumalai@students.olin.edu>
2015-12-09 23:17:27 -0500

    Commenting code

--- Current Interaction/janis/janis.ino
+++ Current Interaction/janis/janis.ino
@@ -13,7 +13,10 @@ State janis;
 bool sensor_vals[NUM_SENSORS];
 
 
-//-----------------------------------------------------------------------------Main Loops---------------------------------------------------------------------------
+//-----------------------------------------------------------------------------Functions---------------------------------------------------------------------------
+
+//-------Setup Helper Functions--------------
+
 State initializeJanis() {
   State janis;
   memset(janis.pos, 0, NUM_SERVOS);
@@ -28,25 +31,24 @@ void attachServos() {
   }
 }
 
-void setup()
-{
-  Serial.begin(9600);
-  memset(sensor_vals, 0, NUM_SENSORS);
-  janis = initializeJanis();
-}
-
-void getSensorInput() {
-  int threshold;
-  for (int i = 0; i < NUM_SENSORS; i++) {
-    sensor_vals[i] = (analogRead(sensor_pins[i]) > threshold);
-  }
-}
+//----- Get Sensor Input-------------
 
 bool ifDetect() {
+  //returns true if sensor_vals contains at least one true
   for (int i = 0; i <NUM_SENSORS; i++)
     if (sensor_vals[i]) return true;
   return false;
 }
+
+void getSensorInput() {
+  //writes bool of if sensor sensed value or not, onto sensor_vals
+  int threshold = 150;
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    sensor_vals[i] = (analogRead(sensor_pins[i]) >= threshold);
+  }
+}
+
+//------Servo Control Helper Functions------------------
 
 bool ifChangeState(int servo_num) {
   //if a servo needs to change direction
@@ -56,17 +58,22 @@ bool ifChangeState(int servo_num) {
 }
 
 void changeDir(int servo_num) {
+  //changing direction
   janis.is_reverse[servo_num] = (! janis.is_reverse[servo_num] );
 }
 
 void increment(int servo_num) {
+  //incrementing by one in the direction of state
   if (janis.is_reverse[servo_num])
     janis.pos[servo_num] -= 1;
   else
     janis.pos[servo_num] += 1;
 }
 
+//------Types of Control Patterns----------------------
+
 void sweepBackForthControl(int sensor_num) {
+  //ccontinously sweep back and forth between 0 and 90
   int servo_pair[] = {(sensor_num * 2), (sensor_num * 2) + 1};
   for (int i = 0; i < sizeof(servo_pair); i ++) {
     if (ifChangeState(servo_pair[i]))
@@ -76,9 +83,21 @@ void sweepBackForthControl(int sensor_num) {
   }
 }
 
-void updateSensorState() {
+void proportionalControl(int sensor_num) {
+
+}
+
+void sweepToNinety(int sensor_num) {
+
+}
+
+//--------Update State------------------
+
+void updateSensorState( void (*f)(int) ) {
+  //if a sensor sees something, run the given control function
   for (int i = 0; i < NUM_SENSORS; i++) {
-    if (sensor_vals[i]) sweepBackForthControl(i);
+    if (sensor_vals[i])
+      (*f)(i);
   }
 }
 
@@ -86,9 +105,20 @@ void updateTimeState() {
 
 }
 
+//--------Display State-------------------
+
 void displayState() {
   for (int i = 0; i < NUM_SERVOS; i++)
     servos[i].write(janis.pos[i]);
+}
+
+//-----------------------------------------------------------------Arduino Functions---------------------------------------------------------------------------------
+
+void setup()
+{
+  Serial.begin(9600);
+  memset(sensor_vals, 0, NUM_SENSORS);
+  janis = initializeJanis();
 }
 
 void loop()
@@ -96,7 +126,7 @@ void loop()
   getSensorInput();
 
   if (ifDetect())
-    updateSensorState();
+    updateSensorState(sweepBackForthControl);
   else
     updateTimeState();
 
